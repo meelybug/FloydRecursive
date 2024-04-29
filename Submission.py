a6de896d1c148180ac14fe7ed80f961b8c0385a7
Caitlin <sgceldri@liverpool.ac.uk>
2024-04-29 17:40:00 +0100

    Commit 3 - function continued

--- Submission.py
+++ Submission.py
@@ -4,3 +4,5 @@ n = 3
 inf = 99999
 # Define the function and the parameters
 def FloydRecursive(graph)
+    # Define distance as the shortest route between each node
+    dist = list(map(lambda i: list(map(lambda j: j, i)), graph))
