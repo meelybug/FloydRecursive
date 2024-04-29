637e9a702ac21bd2962f4bc575cd62421a356365
Caitlin <sgceldri@liverpool.ac.uk>
2024-04-29 17:46:22 +0100

    Commit 4 - function continued 2

--- Submission.py
+++ Submission.py
@@ -3,6 +3,15 @@ n = 3
 # Use infinity as the large enough value
 inf = 99999
 # Define the function and the parameters
-def FloydRecursive(graph)
+def FloydRecursive(graph):
     # Define distance as the shortest route between each node
     dist = list(map(lambda i: list(map(lambda j: j, i)), graph))
+    # Find the distances between node using each of the nodes as the source node
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                # Account for k being between i and j on shortest distance path
+                dist[i][j] = min(dist[i][j],
+                                 dist[i][k] + dist [k][j]
+                                 )
+
